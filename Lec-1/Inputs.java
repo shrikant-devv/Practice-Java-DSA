6f2be33780918d333bd0a933fcdf65edc57ac6fe
shrikant-devv <shrikantchinchole11@gmail.com>
2026-02-15 19:11:57 +0530

    Added Java practice files

---
+++ Lec-1/Inputs.java
@@ -0,0 +1,16 @@
+// package Practice-Java-DSA.Lec-1;
+import java.util.Scanner;
+    public class Inputs {
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+//        System.out.print("Please enter some input: ");
+//        int rollno = input.nextInt();
+//        System.out.println("Your roll number is " + rollno);
+        System.out.print("Enter your name: ");
+        String name = input.next();
+        System.out.println(name);
+
+//        float marks = input.nextFloat();
+//        System.out.println(marks);
+        }
+    }
